bbf2c38d0a11df2129edf53a5f3c708a3936875c
Tyler Jurecki <jurecki07@yahoo.com>
2017-09-19 13:48:41 -0500

    almost complete

--- src/EmailNotification.java
+++ src/EmailNotification.java
@@ -1,4 +1,3 @@
-
 import java.time.LocalDateTime;
 
 
@@ -9,5 +8,27 @@ public class EmailNotification extends Notification{
 
 //    Get pauls help
 
+    public EmailNotification(LocalDateTime createdAt, String subject, String body, String recipient, String smtpProvider) {
+        super(createdAt, subject, body);
+        this.recipient = recipient;
+        this.smtpProvider = smtpProvider;
+        this.status = "email";
+    }
 
+    @Override
+    public void transport() throws NoTransportException {
+        System.out.println(getRecipient());
+        System.out.println(getSmtpProvider());
+        System.out.println(getSubject());
+        System.out.println(getBody());
+        System.out.println(getCreatedAt());
+    }
+
+    public String getRecipient() {
+        return recipient;
+    }
+
+    public String getSmtpProvider() {
+        return smtpProvider;
+    }
 }
--- src/TextNotification.java
+++ src/TextNotification.java
@@ -1,9 +1,6 @@
 import java.time.LocalDateTime;
 
-
-
-
-public class TextNotification {
+public class TextNotification extends Notification {
     private String recipient;
     private String smsProvider;
 
@@ -16,7 +13,23 @@ public class TextNotification {
         System.out.println(getCreatedAt());
     }
 
-//    Generate All in Constructor
+    public TextNotification(LocalDateTime createdAt, String subject, String body, String recipient, String smsProvider) {
+        super(createdAt, subject, body);
+        this.recipient = recipient;
+        this.smsProvider = smsProvider;
 
+    }
+
+    public String getRecipient() {
+        return recipient;
+    }
+
+    public String getSmsProvider() {
+        return smsProvider;
+    }
 }
+
+
+
+
 
